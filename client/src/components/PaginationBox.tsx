24893a0e96572e83bb2798ff3b33fb9466dff636
Luis Renteria <luis.renteria@encora.com>
2024-09-19 13:16:03 -0600

    Fixed bgcolor Pagination

--- client/src/components/PaginationBox.tsx
+++ client/src/components/PaginationBox.tsx
@@ -1,23 +1,32 @@
 import Box from "@mui/material/Box";
-import { Pagination } from "@mui/material";
+import { Pagination, Paper } from "@mui/material";
 import getTodos from "../api/getTodos";
 import { useData } from "../hooks/useData";
 import { useEffect, useState } from "react";
 function PaginationBox() {
-  const { setMetrics,pages, setPages, setRows,updateData ,filterPriority,filterState,searchQuery} = useData();
+  const {
+    setMetrics,
+    pages,
+    setPages,
+    setRows,
+    updateData,
+    filterPriority,
+    filterState,
+    searchQuery,
+  } = useData();
   const [clickedPage, setClickedPage] = useState<number>(1);
 
   useEffect(() => {
-    getTodos(clickedPage,filterPriority,filterState,searchQuery)
+    getTodos(clickedPage, filterPriority, filterState, searchQuery)
       .then((response) => {
         if (response) {
           setPages(response.pages);
           setRows(response.todos);
-          setMetrics(response.metrics)
+          setMetrics(response.metrics);
         }
       })
       .catch(console.error);
-  }, [clickedPage,updateData,filterPriority,filterState]);
+  }, [clickedPage, updateData, filterPriority, filterState]);
 
   const handleChange = async (_event: any, clickedPage: number) => {
     setClickedPage(clickedPage);
@@ -31,14 +40,17 @@ function PaginationBox() {
         padding: "1rem",
       }}
     >
-      <Pagination
-        size={"large"}
-        count={pages.totalPages}
-        page={pages.actualPage}
-        onChange={handleChange}
-        variant={"outlined"}
-        shape={"rounded"}
-      />
+      <Paper sx={{ padding: 0.5 }}>
+        <Pagination
+          size={"large"}
+          count={pages.totalPages}
+          page={pages.actualPage}
+          onChange={handleChange}
+          variant={"outlined"}
+          shape={"rounded"}
+          color="secondary"
+        />
+      </Paper>
     </Box>
   );
 }
