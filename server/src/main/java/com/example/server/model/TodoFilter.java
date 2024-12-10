655a136b901b45ea71df0df7a65fa7c75cbb928d
Luis Renteria <luis.renteria@encora.com>
2024-12-10 13:32:20 -0600

    Create TodoFilter.java

---
+++ server/src/main/java/com/example/server/model/TodoFilter.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.server.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author luis.renteria
+ */
+@Getter
+@Setter
+public class TodoFilter {
+
+    private int page;
+    private String priority;
+    private String state;
+    private String search;
+
+    // Constructor
+    public TodoFilter(int page, String priority, String state, String search) {
+        this.page = page;
+        this.priority = priority;
+        this.state = state;
+        this.search = search;
+
+    }
+
+}
